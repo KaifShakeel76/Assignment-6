9a983bf287b7a02320621176f639a1e7af09d513
KaifShakeel76 <kaifshakeel555@gmail.com>
2024-03-29 02:38:21 +0530

    Updated

--- src/kafka/kafka.groovy
+++ src/kafka/kafka.groovy
@@ -1,11 +1,13 @@
-// Clone repo
-sh "git clone https://github.com/KaifShakeel76/Assignment-6.git"
-
-// User approval
-input message: 'Do you approve this build?', ok:'Proceed'
-
-// Ansible Playbook
-sh "ansiblePlaybook become: true, disableHostKeyChecking: true, installation: 'Ansible', inventory: '/var/lib/jenkins/workspace/TestShared/Tool_Manager/inventory', playbook: '/var/lib/jenkins/workspace/TestShared/Tool_Manager/test.yml', vaultTmpPath: ''"
-
-// Slack Notification
-slackSend channel: 'jenkinsintegration', color: 'Green', message: 'Build Success', teamDomain: 'bhavneshpvt', tokenCredentialId: 'Slack'
+def call(config) {
+    // Clone repo
+    sh "git clone https://github.com/KaifShakeel76/Assignment-6.git"
+    
+    // User approval
+    input message: 'Do you approve this build?', ok:'Proceed'
+    
+    // Ansible Playbook
+    sh "ansiblePlaybook become: true, disableHostKeyChecking: true, installation: 'Ansible', inventory: '/var/lib/jenkins/workspace/TestShared/Tool_Manager/inventory', playbook: '/var/lib/jenkins/workspace/TestShared/Tool_Manager/test.yml', vaultTmpPath: ''"
+    
+    // Slack Notification
+    slackSend channel: 'jenkinsintegration', color: 'Green', message: 'Build Success', teamDomain: 'bhavneshpvt', tokenCredentialId: 'Slack'
+}
